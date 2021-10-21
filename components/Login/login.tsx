1e1f49fbca237b2b4b1105f8b01ae7131a9bc64a
Giwa Uthman <giwauthman8@gmail.com>
2021-10-21 20:47:10 +0100

    trim logo and uodate view

--- components/Login/login.tsx
+++ components/Login/login.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Text, View, TextInput, TouchableNativeFeedback, TouchableHighlight  } from 'react-native';
+import { Text, View, TextInput, Image , TouchableHighlight,ImageBackground,Button  } from 'react-native';
 import { LoginPageProps } from '../../core/Models/props/loginPageProps';
 import { State } from '../../core/Models/state';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { StyleSheet } from 'react-native';
+const logoUrl = require('../../assets/images/logo.png');
+const backgroundUrl = require('../../assets/images/motherschild.png');
 
 
 export class Login extends React.Component<LoginPageProps, State> {
@@ -26,17 +28,25 @@ export class Login extends React.Component<LoginPageProps, State> {
   }
 
   setEmail(email: string){
-    this.email
+    this.email = email;
   }
 
   setPass(password: string){
-    
+    this.password = password;
   }
 
   render() {
     return (
       <View style={styles.root}>
-      <Text style={styles.login_text_font}>Login</Text>
+        <ImageBackground source={backgroundUrl} resizeMode="cover" style={styles.container}>
+
+        <Image style={styles.logo_area} source={logoUrl}/>
+        <View style={styles.form_background}>
+
+          
+      <Text style={styles.login_text_font}>Welcome to child connect</Text>
+      <Text style={[styles.text_center, styles.login_text_color]}>Enter your email and password</Text>
+
       <View style={styles.form_position}>
         <Icon name="email" size={30} />
         <TextInput style={styles.form_input_style}
@@ -67,6 +77,21 @@ export class Login extends React.Component<LoginPageProps, State> {
           secureTextEntry={true}
         />
       </View>
+
+      <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={() => {}}
+            title="Forgot username"
+          />
+          <Button
+            onPress={() => {}}
+            title="Forgot password"
+            color="#841584"
+          />
+        </View>
+
+        <Text style={[styles.text_center, styles.login_text_color]}>If you dont have one create a new user here</Text>
+
       <View style={styles.row_container}>
         <TouchableHighlight activeOpacity={0.8} underlayColor="#ffffff">
           <View style={styles.button_position}>
@@ -74,6 +99,10 @@ export class Login extends React.Component<LoginPageProps, State> {
           </View>
         </TouchableHighlight>
       </View>
+
+        </View>
+          </ImageBackground>
+        
     </View>
     );
   }
@@ -82,17 +111,40 @@ export class Login extends React.Component<LoginPageProps, State> {
 // styles
 const styles = StyleSheet.create({
   root: {    
-    padding: 20,
-    backgroundColor: '#133E7C',
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  container:{
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+
+  logo_area: {
+    marginTop: 50,
+    marginLeft: 20,
+    alignSelf: 'flex-start',
+    height:100,
+    width:100,
+    borderRadius: 10
+  },
+
+  text_center: {
+    textAlign: 'center'
   },
 
   login_text_font: {
+    marginTop: 30,
     alignSelf: 'center',
     fontSize: 30,
-    marginBottom: 50,
-    color: '#9BE6DE',
+    marginBottom: 30,
+    color: '#FC0505',
     fontWeight: '600',
   },
 
@@ -108,6 +160,18 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
   },
 
+  form_background: {  
+    marginTop: 100,  
+    padding:20,
+    marginLeft:20,
+    marginRight: 20,
+    height: 400,
+    opacity: .8,
+    backgroundColor:'#E6E6E6',
+    borderRadius: 20
+  },
+
+
   row_container: {
     alignSelf: 'center',
     marginTop: 30,
@@ -120,11 +184,11 @@ const styles = StyleSheet.create({
     elevation: 8,
     shadowColor: '#0047BB',
     shadowRadius: 8,
-    backgroundColor: '#0D2534',
+    backgroundColor: '#F06403',
   },
 
   login_text_color: {
-    color: '#9BE6DE'
+    color: '#000000'
   }
   
 });
