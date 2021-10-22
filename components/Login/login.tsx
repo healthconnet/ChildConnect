ef55fa4025529c5f6c201dbef6f7c0753fc7fd64
Giwa Uthman <giwauthman8@gmail.com>
2021-10-22 18:08:14 +0100

    Update changes

--- components/Login/login.tsx
+++ components/Login/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TextInput, Image , TouchableHighlight,ImageBackground,Button  } from 'react-native';
+import { Text, View, TextInput, Image, TouchableHighlight, ImageBackground, Button } from 'react-native';
 import { LoginPageProps } from '../../core/Models/props/loginPageProps';
 import { State } from '../../core/Models/state';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -22,117 +22,156 @@ export class Login extends React.Component<LoginPageProps, State> {
 
     if (this.state.isUserLoggedIn) {
     }
-    else{
-        
+    else {
+
     }
   }
 
-  setEmail(email: string){
+  setEmail(email: string) {
     this.email = email;
   }
 
-  setPass(password: string){
+  setPass(password: string) {
     this.password = password;
   }
 
   render() {
     return (
       <View style={styles.root}>
-        <ImageBackground source={backgroundUrl} resizeMode="cover" style={styles.container}>
 
-        <Image style={styles.logo_area} source={logoUrl}/>
-        <View style={styles.form_background}>
-
-          
-      <Text style={styles.login_text_font}>Welcome to child connect</Text>
-      <Text style={[styles.text_center, styles.login_text_color]}>Enter your email and password</Text>
-
-      <View style={styles.form_position}>
-        <Icon name="email" size={30} />
-        <TextInput style={styles.form_input_style}
-          onChangeText={text => this.setEmail(text)} 
-          value={this.email} 
-          textContentType={'emailAddress'}
-          autoCompleteType={'email'}
-          clearButtonMode={'while-editing'}
-          keyboardType={'email-address'}
-          returnKeyLabel={'next'}
-          returnKeyType={'next'}
-          underlineColorAndroid={'#FF0000'}
-          placeholder={'Email Address'}
-        />
-      </View>
-      <View style={styles.form_position}>
-        <Icon name="lock" size={30} />
-        <TextInput style={styles.form_input_style}
-          onChangeText={text => this.setPass(text)} 
-          value={this.password} 
-          textContentType={'password'} 
-          autoCompleteType={'password'}
-          clearButtonMode={'while-editing'}
-          returnKeyLabel={'done'}
-          returnKeyType={'done'}
-          underlineColorAndroid={'#FF0000'}
-          placeholder={'Password'}
-          secureTextEntry={true}
-        />
-      </View>
-
-      <View style={styles.alternativeLayoutButtonContainer}>
-          <Button
-            onPress={() => {}}
-            title="Forgot username"
-          />
-          <Button
-            onPress={() => {}}
-            title="Forgot password"
-            color="#841584"
-          />
-        </View>
-
-        <Text style={[styles.text_center, styles.login_text_color]}>If you dont have one create a new user here</Text>
-
-      <View style={styles.row_container}>
-        <TouchableHighlight activeOpacity={0.8} underlayColor="#ffffff">
-          <View style={styles.button_position}>
-            <Text style={styles.login_text_color}>Login</Text>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Image style={styles.logo_area} source={logoUrl} />
+          <View style={[styles.textAlignStart]}>
+            <Text style={styles.bigFontText} >WELCOME TO CHILD CONNECT</Text>
           </View>
-        </TouchableHighlight>
-      </View>
-
         </View>
-          </ImageBackground>
-        
-    </View>
+        <View style={styles.roundEdges}>
+          <Image style={styles.pageDividerArea} source={backgroundUrl} />
+
+
+
+          <View style={styles.form_background}>
+            <Text style={[styles.text_center, styles.login_text_color, {marginBottom: 20, fontWeight: '400'}]}>Enter your email and password</Text>
+
+            <View style={styles.form_position}>
+              <Icon name="email" size={20} />
+              <TextInput style={styles.form_input_style}
+                onChangeText={text => this.setEmail(text)}
+                value={this.email}
+                textContentType={'emailAddress'}
+                autoCompleteType={'email'}
+                clearButtonMode={'while-editing'}
+                keyboardType={'email-address'}
+                returnKeyLabel={'next'}
+                returnKeyType={'next'}
+                underlineColorAndroid={'#FF0000'}
+              />
+            </View>
+            <View style={styles.form_position}>
+              <Icon name="lock" size={20} />
+              <TextInput style={styles.form_input_style}
+                onChangeText={text => this.setPass(text)}
+                value={this.password}
+                textContentType={'password'}
+                autoCompleteType={'password'}
+                clearButtonMode={'while-editing'}
+                returnKeyLabel={'done'}
+                returnKeyType={'done'}
+                underlineColorAndroid={'#FF0000'}
+                placeholder={'Password'}
+                secureTextEntry={true}
+              />
+            </View>
+
+            
+
+          <View style={styles.twoRowColumns}>
+            <Button
+              onPress={() => { }}
+              title="Forgot username"
+            />
+            <Button
+              onPress={() => { }}
+              title="Forgot password"
+              color="#841584"
+            />
+          </View>
+
+          <Text style={[styles.text_center, styles.login_text_color]}>If you dont have one create a new user here</Text>
+
+          <View style={styles.row_container}>
+            <TouchableHighlight activeOpacity={0.8} underlayColor="#ffffff">
+              <View style={styles.button_position}>
+                <Text style={styles.login_text_color}>Login</Text>
+              </View>
+            </TouchableHighlight>
+          </View>
+
+          </View>
+        </View>
+
+      </View>
     );
   }
 }
 
+
+
 // styles
 const styles = StyleSheet.create({
-  root: {    
+  root: {
     flex: 1,
     justifyContent: 'flex-start',
+    backgroundColor: '#FF553E'
+  },
+
+  roundEdges: {
+    borderRadius: 20
+  },
+
+  textAlignStart: {
+    marginTop: 35,
+    marginLeft: 20
   },
 
   alternativeLayoutButtonContainer: {
-    margin: 20,
+    marginLeft: 15,
+    marginTop: 40,
     flexDirection: 'row',
-    justifyContent: 'space-between'
   },
 
-  container:{
+  twoRowColumns:{
+    flexDirection: 'row',
+    justifyContent:'space-between'
+  },
+
+  bigFontText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: '600'
+  },
+
+  container: {
     flex: 1,
     justifyContent: 'flex-start',
   },
 
   logo_area: {
-    marginTop: 50,
+    height: 100,
+    width: 100
+  },
+
+  pageDividerArea: {
+    maxWidth: 370,
     marginLeft: 20,
-    alignSelf: 'flex-start',
-    height:100,
-    width:100,
-    borderRadius: 10
+    marginTop: 33,
+    maxHeight: 350,
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+  },
+
+  formLogoArea: {
+    width: 300
   },
 
   text_center: {
@@ -148,7 +187,7 @@ const styles = StyleSheet.create({
     fontWeight: '600',
   },
 
-  form_position:{
+  form_position: {
     flexDirection: 'row',
     marginBottom: 20,
   },
@@ -160,15 +199,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
   },
 
-  form_background: {  
-    marginTop: 100,  
-    padding:20,
-    marginLeft:20,
+  form_background: {
+    borderBottomRightRadius: 15,
+    borderBottomLeftRadius: 15,
+    maxWidth: 370,
+    height: 300,
+    padding: 20,
+    marginLeft: 20,
     marginRight: 20,
-    height: 400,
-    opacity: .8,
-    backgroundColor:'#E6E6E6',
-    borderRadius: 20
+    backgroundColor: '#E6E6E6',
   },
 
 
@@ -184,11 +223,11 @@ const styles = StyleSheet.create({
     elevation: 8,
     shadowColor: '#0047BB',
     shadowRadius: 8,
-    backgroundColor: '#F06403',
+    backgroundColor: '#4AB516',
   },
 
   login_text_color: {
     color: '#000000'
   }
-  
+
 });
